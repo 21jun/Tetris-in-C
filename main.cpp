cf0fabae515b338192878c1c3f3c91711d90a20f
21jun <21jun7654@gmail.com>
2018-09-15 01:35:55 +0900

    opening scene

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <windows.h>
-
 void SetCurrentCursorPos(int x, int y)
 {
 	COORD pos = { x,y };
@@ -76,19 +75,48 @@ void LoadBlockModel(int arr[4][4], int key) {
 	fclose(f);
 }
 
+void OpeningScene() {
+
+	system("mode con cols=100 lines=24");
+	FILE * f = fopen("opening.txt", "r");
+	char a[10000];
+	int i = 0;
+	while (!feof(f)) {
+		fgets(a, 1024, f);
+		printf("%s", a);
+		Sleep(2);
+	}
+	fclose(f);
+
+	printf("\n");
+	FILE * f2 = fopen("art.txt", "r");
+	while (!feof(f2)) {
+		a[i] = fgetc(f2);
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), i % 2 == 1 ? 8 : 7);
+		printf("%c", a[i++]);
+		Sleep(1);
+	}
+	fclose(f2);
+
+	printf("PRESS ANY KEY TO CONTINUE");
+	getchar();
+	//Sleep(4000);
+	system("cls");
+
+}
+
 
 int main()
 {
 	int blockModel[10][4][4];
-	FILE * f = fopen("block.txt","r");
-	
+
 	LoadBlockModel(blockModel[0], 0);
 	LoadBlockModel(blockModel[1], 1);
 	LoadBlockModel(blockModel[2], 2);
 	LoadBlockModel(blockModel[3], 3);
 
-
-	SetCurrentCursorPos(0, 10);
+	OpeningScene();
+	/*SetCurrentCursorPos(0, 10);
 	ShowBlock(blockModel[0]);
 
 	SetCurrentCursorPos(10, 10);
@@ -98,7 +126,7 @@ int main()
 	ShowBlock(blockModel[2]);
 
 	SetCurrentCursorPos(30, 10);
-	ShowBlock(blockModel[3]);
+	ShowBlock(blockModel[3]);*/
 
 	getchar();
 	return 0;
